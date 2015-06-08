0aa42a1ca7e6790e64b3025e05619b77d46718e8
Sukbeom Kim <sbkim@thealphalabs.com>
2015-06-08 18:54:03 +0900

    Implement with invoking function for transferring gyrosensor data

--- app/src/main/java/thealphalabs/alphaapp/GlassAppMain.java
+++ app/src/main/java/thealphalabs/alphaapp/GlassAppMain.java
@@ -2,6 +2,10 @@ package thealphalabs.alphaapp;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.support.v4.app.FragmentActivity;
@@ -15,15 +19,23 @@ import android.widget.Button;
 
 public class GlassAppMain extends FragmentActivity implements View.OnClickListener, View.OnTouchListener {
     private final String TAG = "GlassAppMain";
-    private IDataTransferService dataTransferService = ((AlphaApplication)getApplication()).getDataTransferService();
+    private IDataTransferService dataTransferService;
     // Member variables for fragment
     int mCurrentFragmentIndex;
     public final static int FRAGMENT_CONTROLLER = 0;
     public final static int FRAGMENT_APPSTORE   = 1;
 
+    // Member for event handler
+    private SensorManager sensorManager;
+    private SensorEventListener accL;
+    private SensorEventListener gyroL;
+    Sensor accSensor;
+    Sensor gyroSensor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        dataTransferService = ((AlphaApplication) getApplication()).getDataTransferService();
         setContentView(R.layout.activity_glass_app_main);
 
         // Fragment
@@ -34,6 +46,14 @@ public class GlassAppMain extends FragmentActivity implements View.OnClickListen
 
         mCurrentFragmentIndex = FRAGMENT_CONTROLLER;
         fragmentReplace(mCurrentFragmentIndex);
+
+        // Sensor
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        accL = new accListener();
+
+        gyroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        gyroL = new gyroListener();
     }
 
     @Override
@@ -41,6 +61,22 @@ public class GlassAppMain extends FragmentActivity implements View.OnClickListen
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_glass_app_main, menu);
         return true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        sensorManager.registerListener(accL, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(gyroL, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        sensorManager.unregisterListener(accL);
+        sensorManager.unregisterListener(gyroL);
     }
 
     @Override
@@ -110,20 +146,55 @@ public class GlassAppMain extends FragmentActivity implements View.OnClickListen
     }
 
     public boolean onTouch(View v, MotionEvent event) {
-        Log.d(TAG, "x = " + event.getX() + ", y = " + event.getY());
+        Log.d(TAG, "[onTouch] x = " + event.getX() + ", y = " + event.getY());
 
         return true;
     }
 
     public boolean onTouchEvent(MotionEvent event) {
-        Log.d(TAG, "x = " + event.getX() + ", y = " + event.getY());
+        Log.d(TAG, "[onTouchEvent] x = " + event.getX() + ", y = " + event.getY());
 
-        try {
-            dataTransferService.transferMouseData(event.getX(), event.getY());
-        }
-        catch (RemoteException ex) {
-            Log.e(TAG, "RemoteException occurred: " + ex);
-        }
+        // When the IDataTransferService is fully implemented, uncomment following block:
+//        try {
+//            dataTransferService.transferMouseData(event.getX(), event.getY());
+//        }
+//        catch (RemoteException ex) {
+//            Log.e(TAG, "RemoteException occurred: " + ex);
+//        }
         return super.onTouchEvent(event);
+    }
+
+    // Classes for SensorEventListener
+    private class accListener implements SensorEventListener {
+        public void onSensorChanged(SensorEvent event) {
+            Log.d(TAG, "[accelometer event] x = " + event.values[0] + ", y = " + event.values[1] + ", z = " + event.values[2]);
+            // When the IDataTransferService is fully implemented, uncomment following block:
+//        try {
+//            dataTransferService.transferAccelData(event.getX(), event.getY());
+//        }
+//        catch (RemoteException ex) {
+//            Log.e(TAG, "RemoteException occurred: " + ex);
+//        }
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    }
+
+    private class gyroListener implements SensorEventListener {
+        public void onSensorChanged(SensorEvent event) {
+            Log.d(TAG, "[gyroscope event] x = " + event.values[0] + ", y = " + event.values[1] + ", z = " + event.values[2]);
+            // When the IDataTransferService is fully implemented, uncomment following block:
+//        try {
+//            dataTransferService.transferGyroData(event.getX(), event.getY());
+//        }
+//        catch (RemoteException ex) {
+//            Log.e(TAG, "RemoteException occurred: " + ex);
+//        }
+        }
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
     }
 }
