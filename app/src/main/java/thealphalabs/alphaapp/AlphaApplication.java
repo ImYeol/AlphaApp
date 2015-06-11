ac49e557a1c7b738ce4254707994fc40e658c106
ImYeol <gyl115@naver.com>
2015-06-11 16:39:02 +0900

    nice

--- app/src/main/java/thealphalabs/alphaapp/AlphaApplication.java
+++ app/src/main/java/thealphalabs/alphaapp/AlphaApplication.java
@@ -7,32 +7,18 @@ import android.content.ServiceConnection;
 import android.os.IBinder;
 
 /**
- * Created by AlphaLabs on 15. 6. 8.
+ * Created by yeol on 15. 6. 8.
  */
 public class AlphaApplication extends Application {
 
     private final String TAG="AlphaApplication";
-<<<<<<< Updated upstream
-
-    private IDataTransferService DataTransferService;
-=======
     private final DataTransferService transferService= DataTransferService.getInstance(this);
->>>>>>> Stashed changes
     private ServiceConnection mConnection = new ServiceConnection() {
 
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             // TODO Auto-generated method stub
             DataTransferService=IDataTransferService.Stub.asInterface(service);
-<<<<<<< Updated upstream
-//            try {
-//
-//            } catch (RemoteException e) {
-//                // TODO Auto-generated catch block
-//                Log.d(TAG,e.getMessage());
-//            }
-=======
->>>>>>> Stashed changes
         }
 
         @Override
@@ -44,24 +30,13 @@ public class AlphaApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-<<<<<<< Updated upstream
-        // Services
-//        startService(new Intent(this, DataTransferManager.class));
-//        bindService(new Intent(this, DataTransferManager.class), mConnection, BIND_AUTO_CREATE);
-=======
->>>>>>> Stashed changes
     }
 
     @Override
     public void onTerminate() {
         super.onTerminate();
-<<<<<<< Updated upstream
-//        unbindService(mConnection);
-//        stopService(new Intent(this, DataTransferManager.class));
-=======
         unbindService(mConnection);
         stopService(new Intent(this, DataTransferService.class));
->>>>>>> Stashed changes
     }
 
     public DataTransferService getDataTransferService()
--- app/src/main/java/thealphalabs/alphaapp/GlassAppMain.java
+++ app/src/main/java/thealphalabs/alphaapp/GlassAppMain.java
@@ -1,98 +1,39 @@
 package thealphalabs.alphaapp;
 
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.app.Notification;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.PictureDrawable;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import java.lang.reflect.Field;
 
 
-public class GlassAppMain extends FragmentActivity implements View.OnClickListener {
-    private final String TAG = "GlassAppMain";
-    private String tabs[] = {"Controller", "Appstore", "About"};
-
-    // Member variables for fragment
-    private int mCurrentFragmentIndex;
-    public final static int FRAGMENT_CONTROLLER = 0;
-    public final static int FRAGMENT_APPSTORE   = 1;
-
+public class GlassAppMain extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glass_app_main);
 
-<<<<<<< Updated upstream
-        // ActionBar
-        ActionBar actionBar = getActionBar();
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.rgb(43, 68, 140)));
-        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        actionBar.setCustomView(R.layout.custom_actionbar);
-
-        // Fragment
-        Button bt_controller = (Button) findViewById(R.id.bt_controller);
-        Button bt_appstore   = (Button) findViewById(R.id.bt_appstore);
-        bt_controller.setOnClickListener(this);
-        bt_appstore.setOnClickListener(this);
-
-        mCurrentFragmentIndex = FRAGMENT_CONTROLLER;
-        fragmentReplace(mCurrentFragmentIndex);
-
-=======
->>>>>>> Stashed changes
-    }
-
-    public void fragmentReplace(int reqNewFragmentIndex) {
-        Fragment newFragment;
-
-        newFragment = getFragment(reqNewFragmentIndex);
-        final FragmentTransaction transaction =
-                        getFragmentManager().beginTransaction();
-
-        transaction.replace(R.id.fragment_content, newFragment);
-        transaction.commit();
-    }
-    private Fragment getFragment(int idx) {
-        Fragment newFragment = null;
-
-        switch (idx) {
-            case FRAGMENT_CONTROLLER:
-                newFragment = new FragmentController();
-                break;
-            case FRAGMENT_APPSTORE:
-                newFragment = new FragmentAppstore();
-                break;
-            default:
-                Log.d(TAG, "Unexpected case");
-                break;
-        }
-        return newFragment;
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.bt_controller:
-                mCurrentFragmentIndex = FRAGMENT_CONTROLLER;
-                fragmentReplace(mCurrentFragmentIndex);
-                break;
-            case R.id.bt_appstore:
-                mCurrentFragmentIndex = FRAGMENT_APPSTORE;
-                fragmentReplace(mCurrentFragmentIndex);
-                break;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_glass_app_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
 }
