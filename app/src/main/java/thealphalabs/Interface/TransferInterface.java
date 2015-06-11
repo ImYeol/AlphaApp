b59ff3f3573392dc577032fa8e1b3b707aafc4db
ImYeol <gyl115@naver.com>
2015-06-11 17:40:12 +0900

    forgot to change

---
+++ app/src/main/java/thealphalabs/Interface/TransferInterface.java
@@ -0,0 +1,7 @@
+package thealphalabs.Interface;
+
+/**
+ * Created by yeol on 15. 6. 9.
+ */
+public interface TransferInterface {
+}
--- app/src/main/java/thealphalabs/alphaapp/AlphaApplication.java
+++ app/src/main/java/thealphalabs/alphaapp/AlphaApplication.java
@@ -13,30 +13,18 @@ public class AlphaApplication extends Application {
 
     private final String TAG="AlphaApplication";
     private final DataTransferService transferService= DataTransferService.getInstance(this);
-    private ServiceConnection mConnection = new ServiceConnection() {
 
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            // TODO Auto-generated method stub
-            DataTransferService=IDataTransferService.Stub.asInterface(service);
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            DataTransferService=null;
-        }
-
-    };
     @Override
     public void onCreate() {
         super.onCreate();
+
     }
 
     @Override
     public void onTerminate() {
         super.onTerminate();
-        unbindService(mConnection);
-        stopService(new Intent(this, DataTransferService.class));
+        transferService.StopBluetoothConnection();
+        transferService.StartWifiP2PConnection();
     }
 
     public DataTransferService getDataTransferService()
